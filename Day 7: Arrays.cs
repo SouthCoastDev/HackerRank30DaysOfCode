4dc0b68bbebcd7053b4326c81ab1b631ab7be65d
Alex <alexanderchapman@outlook.com>
2021-07-23 18:09:04 +0100

    Update Day 7: Arrays.cs

--- Day 7: Arrays.cs
+++ Day 7: Arrays.cs
@@ -5,6 +5,7 @@ class Solution
         int n = Convert.ToInt32(Console.ReadLine().Trim());
 
         List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+        // I know it says array but the challange makes it a list!
     
         arr.Reverse();
         var retVal = string.Join(" ", arr);
